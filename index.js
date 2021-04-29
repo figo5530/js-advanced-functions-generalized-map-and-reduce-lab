2669327ad8f54bc46bd362eee1b699362a4d41a5
figo5530 <figooooo0221@gmail.com>
2021-04-29 22:17:52 +0800

    commit tree 138269c82de5278b78b9279fab659e4680b235af

--- index.js
+++ index.js
@@ -1,1 +1,25 @@
 // Add your functions here
+function map(arr, cb) {
+    const result = []
+    for (let i = 0; i < arr.length; i++) {
+        result.push(cb(arr[i]))        
+    }
+    return result
+}
+
+function reduce(arr, cb, startingPoint) {
+    let total
+    if (startingPoint) {
+        total = startingPoint
+        for (let i = 0; i < arr.length; i++) {
+            total = cb(arr[i], total)        
+        }
+        return total
+    } else {
+        total = arr[0]
+        for (let i = 1; i < arr.length; i++) {
+            total = cb(arr[i], total)        
+        }
+        return total
+    }
+}
